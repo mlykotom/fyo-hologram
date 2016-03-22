a583b50b04f9f8ba240efea46a9ae3c179a4fb76
Tomáš Mlynarič <mlynarict@gmail.com>
2016-03-22 17:24:09 +0100

    Added fabric library with basic usage

--- hologram.js
+++
@@ -1,6 +0,0 @@
-
-var canvasElement = document.getElementById('canvas');
-var canvas = canvasElement.getContext("2d");
-
-canvas.fillStyle = "#FF0000";
-canvas.fillRect(0, 0, 150, 75);
---
+++ js/hologram.js
@@ -0,0 +1,15 @@
+
+// create a wrapper around native canvas element (with id="c")
+var canvas = new fabric.Canvas('canvas');
+
+// create a rectangle object
+var rect = new fabric.Rect({
+    left: 100,
+    top: 100,
+    fill: 'red',
+    width: 20,
+    height: 20
+});
+
+// "add" rectangle onto canvas
+canvas.add(rect);
